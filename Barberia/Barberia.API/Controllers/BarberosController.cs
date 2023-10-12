b855d4b9b472e02d5813a8f1bb2590ffedaffa12
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2023-10-12 17:52:49 -0500

    Controlador de la entidad barbero creado (Sin finalizar)

---
+++ Barberia/Barberia.API/Controllers/BarberosController.cs
@@ -0,0 +1,17 @@
+﻿using Barberia.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Barberia.API.Controllers
+{
+    [ApiController]
+    [Route("/api/barberos")]
+    public class BarberosController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BarberosController (DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
