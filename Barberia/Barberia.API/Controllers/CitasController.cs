35035ca10ff0522ba4417d50ad57baca5a8a92d9
SANTIAGO VARELA JIM�NEZ <santyvarela.03@gmail.com>
2023-10-12 18:44:14 -0500

    Se ha creado el controlador con sus respectivos metodos para Citas

---
+++ Barberia/Barberia.API/Controllers/CitasController.cs
@@ -0,0 +1,65 @@
+﻿using Barberia.API.Data;
+using Barberia.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barberia.API.Controllers
+{
+    [ApiController]
+    [Route("/api/citas")]
+    public class CitasController : ControllerBase 
+    {
+         private readonly DataContext _context;
+
+        public CitasController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //Lista de citas 
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Citas.ToListAsync());
+        }
+
+        //Get por parametro
+        [HttpGet("{Id=Id_Cita}")]
+        public async Task<ActionResult> Get(int Id)
+        {
+            var cita = await _context.Citas.FirstOrDefaultAsync(c => c.Id == Id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+            return Ok(cita);
+        }
+
+        [HttpPost]//inserta registros
+        public async Task<IActionResult> Post(Cita cita)
+        {
+            _context.Add(cita);
+            await _context.SaveChangesAsync();// --> guarda en la tabla 
+            return Ok(cita);
+        }
+
+        [HttpPut]// --> update
+        public async Task<IActionResult> Put(Cita cita)
+        {
+            _context.Add(cita);
+            await _context.SaveChangesAsync();
+            return Ok(cita);
+        }
+
+        [HttpDelete("{Id_Cita:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var FilaAfectada = await _context.Citas.Where(c => c.Id == id).ExecuteDeleteAsync();
+            if (FilaAfectada == 0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
