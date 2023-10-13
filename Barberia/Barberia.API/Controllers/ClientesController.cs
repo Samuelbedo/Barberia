d13673b1ab5baafd1c383bd25ae250213b474056
SANTIAGO VARELA JIM�NEZ <santyvarela.03@gmail.com>
2023-10-12 20:53:06 -0500

    Se termina los metodos del controlador cliente

--- Barberia/Barberia.API/Controllers/ClientesController.cs
+++ Barberia/Barberia.API/Controllers/ClientesController.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Barberia.API.Controllers
 {
     [ApiController]
-    [Route("/api/cliente")]
+    [Route("/api/clientes")]
     public class ClientesController : ControllerBase
     {
 
@@ -17,11 +17,51 @@ namespace Barberia.API.Controllers
         {
             _context = context;
         }
-       
+
         [HttpGet]  //Lista de clientes
         public async Task<ActionResult> Get()
         {
             return Ok(await _context.Clientes.ToListAsync());
         }
+
+        [HttpGet("{Id=cedula}")] //Get por parametro
+        public async Task<ActionResult> Get(int Cedula)
+        {
+            var cliente = await _context.Clientes.FirstOrDefaultAsync(c => c.Cedula == Cedula);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return Ok(cliente);
+        }
+
+        [HttpPost]//insertar registros
+        public async Task<IActionResult> Post(Cliente cliente)
+        {
+            _context.Add(cliente);
+            await _context.SaveChangesAsync();//guardar la tabla de clientes
+            return Ok(cliente);
+
+        }
+
+        [HttpPut]//update
+        public async Task<IActionResult> Put(Cliente cliente)
+        {
+            _context.Add(cliente);
+            await _context.SaveChangesAsync();
+            return Ok(cliente);
+        }
+
+        [HttpDelete("{cedula:int}")]
+        public async Task<IActionResult> Delete(int cedula)
+        {
+            var FilaAfectada = await _context.Clientes.Where(c => c.Cedula == cedula).ExecuteDeleteAsync();
+            if (FilaAfectada == 0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
     }
 }
