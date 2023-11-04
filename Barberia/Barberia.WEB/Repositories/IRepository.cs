f2840490dfca9e9f61adb4b1ca7728f07c5385be
SANTIAGO VARELA JIM�NEZ <santyvarela.03@gmail.com>
2023-11-04 11:45:39 -0500

    Se ha modificado el puerto del proyecto.Web, agregado los repositorios y su debida inyeccion de dependencias.

---
+++ Barberia/Barberia.WEB/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Barberia.WEB.Repositories;
+
+namespace Barberia.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+
+
+
+
+
+    }
+}
---
+++ Barberia/Barberia.WEB/Repositories/Repository.cs
@@ -0,0 +1,22 @@
+﻿using Barberia.WEB.Repositories;
+
+namespace Barberia.WEB.Repositories
+{
+    public class Repository : IRepository
+    {
+        public Task<HttpResponseWrapper<T>> Get<T>(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseWrapper<object>> Post<T>(string url, T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
