891153bb5e3293c72b133c4cfeb351aa80a0ce16
SANTIAGO VARELA JIM�NEZ <santyvarela.03@gmail.com>
2023-11-10 15:49:14 -0500

    Se ha creado el controlador para el Put de cada entidad, se ha agregado al Css un arcivo de estilos para modificar la parte visual de la pagina.

---
+++ Barberia/Barberia.API/Migrations/20231109011843_UpdatePutEntities.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Barberia.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePutEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
