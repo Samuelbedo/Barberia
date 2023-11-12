e3d5e372ac2b657a346fdd1e8e1ec20cfb13dbc2
SANTIAGO VARELA JIM�NEZ <santyvarela.03@gmail.com>
2023-11-12 13:34:49 -0500

    Se ha actualizado el SeedDb y modificado para Barbero el Form y Index.

--- Barberia/Barberia.API/Data/SeedDb.cs
+++ Barberia/Barberia.API/Data/SeedDb.cs
@@ -22,13 +22,13 @@ namespace Barberia.API.Data
         {
             if (!_context.Barberos.Any())
             {
-                _context.Barberos.Add(new Barbero { Nombre = " Carlos Conde " });
-                _context.Barberos.Add(new Barbero { Nombre = " Elias Vanhouse " });
-                _context.Barberos.Add(new Barbero { Nombre = " Drake Campbell " });
-                _context.Barberos.Add(new Barbero { Nombre = " Vinicius Mosquera " });
-                _context.Barberos.Add(new Barbero { Nombre = " Aaron Manuel Florez " });
+                _context.Barberos.Add(new Barbero { Nombre = " Carlos Conde ", Especialidad = "Cortes a tijera ", Telefono = 247814 });
+                _context.Barberos.Add(new Barbero { Nombre = " Maria Vanhouse ", Especialidad = "Tratamientos para cabello ", Telefono = 781456 });
+                _context.Barberos.Add(new Barbero { Nombre = " Drake Campbell ", Especialidad = "Cortes Urbanos y barbas", Telefono = 152679 });
+                _context.Barberos.Add(new Barbero { Nombre = " Vinicius Mosquera ", Especialidad = "trenzas y dreadlocks", Telefono = 314697 });
+                _context.Barberos.Add(new Barbero { Nombre = " Aaron Manuel Florez ", Especialidad = "Cortes a tijera y barbas", Telefono = 484828 });
             }
-
+                
             await _context.SaveChangesAsync();
         }
     }
