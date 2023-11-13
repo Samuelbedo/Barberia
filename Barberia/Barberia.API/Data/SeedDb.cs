2d33caa8c607d05484bc4543ff76945f6a2b2a96
SANTIAGO VARELA JIM�NEZ <santyvarela.03@gmail.com>
2023-11-13 14:05:26 -0500

    Se han cambiado los colores para los botones del Index y Form para cada vista

--- Barberia/Barberia.API/Data/SeedDb.cs
+++ Barberia/Barberia.API/Data/SeedDb.cs
@@ -22,11 +22,11 @@ namespace Barberia.API.Data
         {
             if (!_context.Barberos.Any())
             {
-                _context.Barberos.Add(new Barbero { Nombre = " Carlos Conde ", Especialidad = "Cortes a tijera ", Telefono = 247814 });
-                _context.Barberos.Add(new Barbero { Nombre = " Maria Vanhouse ", Especialidad = "Tratamientos para cabello ", Telefono = 781456 });
-                _context.Barberos.Add(new Barbero { Nombre = " Drake Campbell ", Especialidad = "Cortes Urbanos y barbas", Telefono = 152679 });
-                _context.Barberos.Add(new Barbero { Nombre = " Vinicius Mosquera ", Especialidad = "trenzas y dreadlocks", Telefono = 314697 });
-                _context.Barberos.Add(new Barbero { Nombre = " Aaron Manuel Florez ", Especialidad = "Cortes a tijera y barbas", Telefono = 484828 });
+                _context.Barberos.Add(new Barbero { Nombre = " Carlos Conde ", Especialidad = "Cortes con tijera ", Telefono = 247814726 });
+                _context.Barberos.Add(new Barbero { Nombre = " Maria Vanhouse ", Especialidad = "Tratamientos para cabello ", Telefono = 781456354 });
+                _context.Barberos.Add(new Barbero { Nombre = " Drake Campbell ", Especialidad = "Cortes Urbanos y barbas", Telefono = 152679358 });
+                _context.Barberos.Add(new Barbero { Nombre = " Vinicius Mosquera ", Especialidad = "Trenzas y dreadlocks", Telefono = 314697124 });
+                _context.Barberos.Add(new Barbero { Nombre = " Aaron Manuel Florez ", Especialidad = "Cortes con tijera y barbas", Telefono = 484828378 });
             }
                 
             await _context.SaveChangesAsync();
