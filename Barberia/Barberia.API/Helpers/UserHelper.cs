5e257c4404058211b4db0257967adacc34a49abe
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2023-11-24 02:07:16 -0500

    ultimo commit commit

--- Barberia/Barberia.API/Helpers/UserHelper.cs
+++ Barberia/Barberia.API/Helpers/UserHelper.cs
@@ -49,8 +49,27 @@ namespace Barberia.API.Helpers
 
         public async Task<User> GetUserAsync(string email)
         {
-            return await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
+            return user!;
         }
+
+        public async Task<User> GetUserAsync(Guid userId)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == userId.ToString());
+            return user!;
+
+        }
+
+        public async Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword)
+        {
+            return await _userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+        }
+
+        public async Task<IdentityResult> UpdateUserAsync(User user)
+        {
+            return await _userManager.UpdateAsync(user);
+        }
+
 
         public async Task<bool> IsUserInRoleAsync(User user, string roleName)
         {
